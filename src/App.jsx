a7d3ec3ec30f505809620eecb6ad50e6ac93f10f
dynamic109 <adekanbiemmanuel109@gmail.com>
2024-08-12 11:06:44 +0100

    Basic app structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-light">
+      <h1>Sign In Form With Firebase</h1>
+    </div>
+  );
+}
+
+export default App;
